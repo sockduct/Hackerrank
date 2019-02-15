60f584ad6f1aa4f3c1a129d6f4307ae11f901377
James Small <james.r.small@outlook.com>
2019-02-15 17:01:03 -0500

    Update:  basicdatatypes.cpp - add comments

--- C++/basicdatatypes.cpp
+++ C++/basicdatatypes.cpp
@@ -1,6 +1,14 @@
+/*
+ * James Small, 02/15/2019
+ * My solution for HackerRank Challenge:
+ * Practice, C++, Introduction, Basic Data Types
+ */
+// First two needed for C++
 #include <iomanip>
 #include <iostream>
+// This one needed for scanf/printf
 #include <cstdio>
+
 using namespace std;
 
 int main() {
@@ -17,6 +25,7 @@ int main() {
     // Using C++ style:
     // Could also enable commas with:
     // cout.imbue(locale("en_US.utf8"));
+    // Comment this section out by changing // to /* ... */
     //
     cin >> a >> b >> c >> d >> e;
     cout << a << '\n' << b << '\n' << c << '\n' << fixed
@@ -25,6 +34,8 @@ int main() {
     //
     
     // Alternatively using C style:
+    // Enable this section by commenting out C++ style and
+    // changing /* ... */ to //
     /*
     // Hackerrank used %ld to match up with long int
     // Per our adjustment above must use %lld
