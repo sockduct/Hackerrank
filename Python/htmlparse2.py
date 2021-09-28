ed5f655eef89c4dd1257f84cfad207d0c1927a12
James Small <james.r.small@outlook.com>
2021-09-27 22:04:04 -0400

    Add/Update:  HTML Parser - Part 2 challenge solution+
    * Minor updates to HTML Parser - Part 1 challenge solution

--- Python/htmlparse2.py
+++ Python/htmlparse2.py
@@ -9,51 +9,54 @@ example_html = '''
   <body data-modal-target class='1'>
     <h1 class="header">HackerRank</h1>
     <br id="main"/>
+    <!--[if IE 9]>IE9-specific content
+    <![endif]-->
+    <div> Welcome to HackerRank</div>
+    <!--[if IE 9]>IE9-specific content<![endif]-->
   </body>
 </html>
 '''
 
 
 class MyHTMLParser(HTMLParser):
+    '''
     def handle_starttag(self, tag, attrs):
-        '''
-        print(f'      Found start tag:  {tag}')
-        if attrs:
-            print(f'     Found attributes:  {attrs}')
-        '''
         print(f'Start : {tag}')
         for k, v in attrs:
             print(f'-> {k} > {v}')
 
     def handle_endtag(self, tag):
-        # print(f'        Found end tag:  {tag}')
         print(f'End   : {tag}')
 
     # Empty tags:
     def handle_startendtag(self, tag, attrs):
-        '''
-        print(f'   Found an empty tag:  {tag}')
-        if attrs:
-            print(f'     Found attributes:  {attrs}')
-        '''
         print(f'Empty : {tag}')
         for k, v in attrs:
             print(f'-> {k} > {v}')
+    '''
+
+    def handle_data(self, data):
+        if data.strip():
+            # print(f'Encountered some data:  {data}')
+            print(f'>>> Data\n{data}')
+
+    def handle_comment(self, data):
+        if data.strip():
+            lines = data.splitlines()
+            if len(lines) == 1:
+                # print(f'Encountered a comment:  {data}')
+                print(f'>>> Single-line Comment\n{data}')
+            else:
+                print(f'>>> Multi-line Comment\n{data}')
 
 
 def main():
     parser = MyHTMLParser()
-
     lines = int(input())
-    for _ in range(lines):
-        parser.feed(input())
-        # Alternatively, collect all input and then parse:
-        # html += input()
+    data = '\n'.join(input() for _ in range(lines))
 
-    # parser.feed(html)
-    #
-    # Need to explicitly close?
-    # parser.close()
+    parser.feed(data)
+    parser.close()
 
     # Example:
     # parser.feed(example_html)
