1c7009391f0965086db7b330f183c4aa6f42e93d
James Small <james.r.small@outlook.com>
2022-06-30 22:26:19 -0400

    Update:  Continue working on palindromic subsets challenge

--- Python/palinsub.py
+++ Python/palinsub.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python3
 
 
+from itertools import combinations
+import sys
+
+
 def shift(s, start, stop, inc):
     dec = ord('a')  # Code point number for letter 'a'
     letters = 26    # Number of lower case English letters
@@ -13,21 +17,45 @@ def shift(s, start, stop, inc):
     return ''.join(new_list)
 
 
+def is_palindrome(letters):
+    size = len(letters)
+
+    if size == 1:
+        return True
+
+    mid = size // 2
+    return all(letters[i] == letters[-(i + 1)] for i in range(mid))
+
+
 def palindrome_subsets(s, start, stop):
-    ...
+    letters = s[start:stop + 1]
+    number = len(letters)
+
+    if number < 1:
+        return 0
+    elif number == 1:
+        return 1
+
+    # Start from 0 and add 1 for each individual letter:
+    total = number
+    for i in range(2, number + 1):
+        total += sum(is_palindrome(c) for c in combinations(letters, i))
+
+    return total + palindrome_subsets(letters[1:], 0, number - 1)
 
 
 if __name__ == '__main__':
+    verbose = len(sys.argv) == 2 and sys.argv[1] == 'verbose'
     n, q = map(int, input().split())
     s = input()
 
     for _ in range(q):
-        query_type, query_params = map(int, input().split())
+        query_type, *query_params = map(int, input().split())
 
         if query_type == 1:
-            start, stop, inc = query_params
-            shift(s, start, stop, inc)
+            s = shift(s, *query_params)
+            if verbose:
+                print(f's is now "{s}"')
         else:
-            start, stop = query_params
-            res = palindrome_subsets(s, start, stop)
+            res = palindrome_subsets(s, *query_params)
             print(f'{res % (10**9 + 7)}')
