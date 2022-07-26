f09a884f70c1c19dfb74ef0ee612db385e241712
James Small <james.r.small@outlook.com>
2022-07-25 22:59:51 -0400

    Update:  Working solution to Tree - Top View Challenge

--- Python/treetopview.py
+++ Python/treetopview.py
@@ -39,30 +39,90 @@ class BinarySearchTree:
 # Specifications are unclear - for good description of what's desired see:
 # https://www.geeksforgeeks.org/print-nodes-top-view-binary-tree/
 # Note:  Warning (spoiler alert!) - the above link provides solutions!
-#
-# Need to do breadth-first-traversal and figure out what's "visible" from the
-# "top"
-def topView(root):
-    def leftTopView(root):
-        if root and root.left:
-            leftTopView(root.left)
+import queue
 
-            print(f'{root.info} ', end='')
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+        self.x = 0
+        self.y = 0
 
-    def rightTopView(root):
-        if root:
-            print(f'{root.info} ', end='')
+    def __lt__(self, other):
+        return self.info < other.info if isinstance(other, Node) else NotImplemented
 
-            if root.right:
-                rightTopView(root.right)
+    def __gt__(self, other):
+        return self.info > other.info if isinstance(other, Node) else NotImplemented
 
-    leftTopView(root)
+    def __repr__(self):
+        return f'{self.info:3}-<{self.x:3}, {self.y:2}>'
 
-    if root:
-        print(f'{root.info} ', end='')
+    def __str__(self):
+        return f'{self.info}'
 
+
+class BinaryTour:
+  def __init__(self, tree):
+    """Prepare an Euler tour template for given tree."""
+    self._tree = tree
+
+  def execute(self):
+    """Perform the tour and return any result from post visit of root."""
+    if self._tree.root:
+      return self._tour(self._tree.root, 0, [])    # start the recursion
+
+  def _tour(self, root, d, path):
+    if not path:
+        # Root node is horizontal offset 0:
+        path.append(0)
+    if root.left:
+      # Using this to calculate horizontal offset from parent:
+      path.append(path[-1] - 1)
+      self._tour(root.left, d + 1, path)
+      path.pop()
+    self._hook_invisit(root, d, path)                   # "in visit" for p
+    if root.right:
+      # Using this to calculate horizontal offset from parent:
+      path.append(path[-1] + 1)
+      self._tour(root.right, d + 1, path)
+      path.pop()
+
+  def _hook_invisit(self, root, d, path):
+      root.x = 0 if d == 0 else path[-1]
+      root.y = d
+
+
+def breadthfirst(tree):
+    """Generate a breadth-first iteration of the positions of the tree."""
+    if not tree.root:
+        return
+
+    q = queue.SimpleQueue()
+    q.put(tree.root)
+    while not q.empty():
+        root = q.get()
+        yield root
+
+        if root.left:
+            q.put(root.left)
         if root.right:
-            rightTopView(root.right)
+            q.put(root.right)
+
+
+def topView(root):
+    binary_layout = BinaryTour(tree)
+    binary_layout.execute()
+    level = 0
+    elements = [(e, e.x) for e in breadthfirst(tree)]
+    elements.sort(key=lambda e: e[1])
+    tracker = set()
+    for e in elements:
+        if e[1] not in tracker:
+            print(f'{e[0]} ', end='')
+            tracker.add(e[1])
+
 
 
 tree = BinarySearchTree()
