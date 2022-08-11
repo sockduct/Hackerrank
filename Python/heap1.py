e6c1497285c5cfb850742641d675cb4e0350edd1
James Small <james.r.small@outlook.com>
2022-08-10 23:09:02 -0400

    Update:  Continue working on QHeap1 challenge

--- Python/heap1.py
+++ Python/heap1.py
@@ -3,53 +3,70 @@
 
 import heapq
 import sys
+import time
 
 
-def process_queries1(data):
+def process_queries1(heap):
     match list(map(int, input().split())):
         case [1, value]:
             # Add value to heap
-            heapq.heappush(data, value)
+            heapq.heappush(heap, value)
         case [2, value]:
-            # Delete value from heap
-            # Can't use this because may not be smallest element (not exactly
-            # matching a heap data structure)
-            # heapq.heappop(data, value)
-            data.pop(data.index(value))
-            heapq.heapify(data)
+            # Delete arbitrary value from heap
+            if value[0] == heap[0]:
+                heapq.heappop(heap)
+            else:
+                # Better way?
+                heap.pop(heap.index(value[0]))
+                heapq.heapify(heap)
         case [3]:
-            # Print minimum value on heap
-            print(f'{heapq.nsmallest(1, data)[0]}')
+            # Print minimum value on heap - slow:
+            # print(f'{heapq.nsmallest(1, heap)[0]}')
+            # Per source code can use heap[0] to see smallest item on heap:
+            print(f'{heap[0]}')
         case _:
             raise ValueError(f'Unexpected value')
 
 
-def process_queries2(data):
+def process_queries2(heap):
     command, *value = map(int, input().split())
     if command == 1:
         # Add value to heap
-        heapq.heappush(data, value[0])
+        heapq.heappush(heap, value[0])
     elif command == 2:
-        # Delete value from heap
-        data.pop(data.index(value[0]))
-        heapq.heapify(data)
+        # Delete arbitrary value from heap
+        if value[0] == heap[0]:
+            heapq.heappop(heap)
+        else:
+            # Better way?
+            heap.pop(heap.index(value[0]))
+            heapq.heapify(heap)
     elif command == 3:
-        print(f'{heapq.nsmallest(1, data)[0]}')
+        # Per source code can use heap[0] to see smallest item on heap:
+        print(f'{heap[0]}')
     else:
         raise ValueError(f'Unexpected value "{command}"')
 
 
 def main():
-    data = []
+    start_time = time.time()
+    count = 0
+    heap = []
     skip = True
 
     queries = int(input())
     for _ in range(queries):
         # Choose one:
         if sys.version_info.minor >= 10 and not skip:
-            process_queries1(data)
+            process_queries1(heap)
         else:
-            process_queries2(data)
+            process_queries2(heap)
+
+        count += 1
+        if count == 50_000:
+            print(f'\nRun time:  {(time.time() - start_time):.3f} seconds', file=sys.stderr, flush=True)
+
+    print(f'\nRun time:  {(time.time() - start_time):.3f} seconds', file=sys.stderr, flush=True)
 
 
 if __name__ == '__main__':
